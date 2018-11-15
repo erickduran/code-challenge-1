64c6c9365fa7ba8a491ed8a21cf90f9fcfcd4e82
erick <erick.duran@cetys.edu.mx>
2018-11-14 20:22:55 -0800

    challenge 2 setup

--- challenge-2/main.py
+++ challenge-2/main.py
@@ -1,7 +1,24 @@
 import cv2
-
+import numpy as np
+	
+# add database to res/ folder inside this one
 def main():
-	print('Hello!')
+	# img = cv2.imread('image.png')
+	count = 1
+	while True:
+		img_name = 'res/girl/'+ '{:05}'.format(count) + '.jpg'
+		frame = cv2.imread(img_name, 1)
+		
+		cv2.imshow('frame', frame)
+
+		# time.sleep(0.05)
+		count += 1
+
+		k = cv2.waitKey(5) & 0xFF 
+		if k == 27 or count == 699:
+			break
+
+	cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    main()
+	main()
